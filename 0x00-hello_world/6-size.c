85d44d1c0564b5b6ac61a9c48fe3feec5d8baa23
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-17 23:19:02 -0700

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@ int main(void)
 {
   printf("Size of a char: %i byte(s)\n",sizeof(char));
   
-  pirntf("Size of an int: %i byte(s)\n",sizeof(int));
- pirntf("Size of a long int: %i byte(s)",sizeof(long int ));
- pirntf("Size of a long long int: %i byte(s)",sizeof(long long int));
- pirntf("Size of a float: %i byte(s)",sizeof(float));
+  printf("Size of an int: %i byte(s)\n",sizeof(int));
+ printf("Size of a long int: %i byte(s)",sizeof(long int ));
+ printf("Size of a long long int: %i byte(s)",sizeof(long long int));
+ printf("Size of a float: %i byte(s)",sizeof(float));
  return 0;
 }
