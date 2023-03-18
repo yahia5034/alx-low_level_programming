168022c088e36aa6fac060add4607ec29daf7644
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-17 23:44:10 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main -Prints the size of the different varaibles
- *Return :Always 0.
+ *main - Prints the size of the different varaibles
+ *Return:Always 0.
  */
 int main(void)
 {
