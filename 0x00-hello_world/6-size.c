ba614549812865f52c5f848ff562cf56cab6ee83
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-17 23:15:38 -0700

    initial

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("Size of a char: %i byte(s)\n",sizeof(char));
+  
+  pirntf("Size of an int: %i byte(s)\n",sizeof(int));
+ pirntf("Size of a long int: %i byte(s)",sizeof(long int ));
+ pirntf("Size of a long long int: %i byte(s)",sizeof(long long int));
+ pirntf("Size of a float: %i byte(s)",sizeof(float));
+ return 0;
+}
