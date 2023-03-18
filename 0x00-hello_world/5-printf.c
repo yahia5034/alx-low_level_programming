5e232535421b02a822528c51102f36f6b9fef72f
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-17 23:36:11 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+11;rgb:0000/0000/0000/**
+ *main - Prints the line
+ *Return: Always 0.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+ return (0);
 }
