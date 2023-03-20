ab1015a2740213ed9fa24f3a4c8086fbb58ad000
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-20 08:57:56 -0700

    alias commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	  }
 	else if(n==0)
 	  { 
-	    printf( "is zero");
+	    printf("is zero");
 	  }
 	else
 	  {
-	     printf"is negative");
+	    printf("is negative");
 	  }
 	return (0);
 }
