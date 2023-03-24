9a0cada8a7766eef082760093ee9250ad810f0e7
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-23 19:02:06 -0700

    alias commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
