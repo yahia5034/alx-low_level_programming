3b6232f819b22fe375e2356551f02c1af9d655fb
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-28 15:14:48 -0700

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - this function reset the vaue to 98
+ *@*n: first input and it is a pointer to a variable
+ * Return: No return 
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
+void reset_to_98(int *n);
+
+#endif
