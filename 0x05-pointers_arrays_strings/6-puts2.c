d8a5ecafb88a93303ed8bbf511cfa97a26bd4395
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-28 17:32:08 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print even
+ * @str: input string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 != 0)
+		       continue;
+		else
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}	
