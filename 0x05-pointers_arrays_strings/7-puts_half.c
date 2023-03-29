26e26339a5f21a31f7869e0685f806fc1091cb91
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-28 18:18:20 -0700

    hope

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,16 @@ void puts_half(char *str)
 	while (str[c] != '\0')
 		c++;
 	if (c % 2 == 0)
-		i = (c - 1) / 2;
-	else
-		i = c / 2;
-	
-	for (i++ ; i < c ; i++); 
 	{
-		_putchar(str[i]);
-		
+		i = (c - 1) / 2;
+	}
+	else
+	{
+		i = c / 2;
+	}
+	for (i++ ; i < c ; i++)
+	{
+		_putchar(str[i]);	
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 /**
  * print_array - print no. of array
- * @a: the array
- * @n: the number 
+ * @a: the array.
+ * @n: the number.
  * Return: none.
  */
 void print_array(int *a, int n)
@@ -11,6 +11,8 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("%d", a[i]);
-	printf("\n");
+		if (i++ != n)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
 }
