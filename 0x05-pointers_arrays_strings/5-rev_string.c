cdc1c19a462054739e7268f4564c17ee727e346a
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-03-28 17:23:48 -0700

    3aaaaaaaa

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse the string
+ * @s: input string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, count = 0;
+	char temp;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	count--;
+	for (i = 0 ; count != i && i < count; i++, count--)
+	{
+		temp = *(s+i);
+		*(s+i) = *(s+count);
+		*(s+count) = temp;
+	}
+	
+}
