b5a6cc809ebe4440b25961a87256a2b6161670ca
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-08 03:29:32 -0700

    first in a while

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+*/
+
+
 void _puts_recursion(char *s)
 {
 	if( s[0] == '\0' )
 	{
+		_putchar( '\n' );
 		return ;
 	}
-	putchar( s[0] );
+	_putchar( s[0] );
 	_puts_recursion( s+1 );
 }
+
