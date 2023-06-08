9ec8b6c6def8953893774266d54742aed924db6e
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-08 03:59:08 -0700

    second

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if(s[0] == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(s[0]);
+}
+
