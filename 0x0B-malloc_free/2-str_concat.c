e2dbc6ed7d98e81e8aaa64289eb3045db4117746
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-12 00:08:25 -0700

    fd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,17 +3,21 @@
  * *str_concat - concatenates two strings.
  * @s1: string
  * @s2: string
- * Return: returns eturned pointer should point to a newly allocated space or NULL
+ * Return: returns eturned pointer should point to a
+ * newly allocated space or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
 	int n1 = 0, n2 = 0;
 	char *n;
 	int x1, x2;
-	
-	while ( s1[n1] != 0)
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[n1] != 0)
 		n1++;
-	while ( s2[n2] != 0)
+	while (s2[n2] != 0)
 		n2++;
 
 	n = malloc(n1 + n2 + 1);
