6a644bd077c45b706334c222f8a36c7cec48097d
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-12 00:04:07 -0700

    std

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,10 +9,14 @@ char *_strdup(char *str)
 {
 	int s = 0;
 	char *n; 
+	
+	if (str == NULL)
+		return (NULL);
+
 	while (str[s] != '\0')
 		s++;
 
-	n = malloc(sizeof(str) + 1);
+	n = malloc(s*sizeof(*str) + 1);
 	
 	if (n == 0)
 		return (n);
