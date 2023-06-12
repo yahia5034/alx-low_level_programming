193246a296ba09e3be5fee460f78e4793a1ffc07
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-12 01:21:25 -0700

    s

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * free_grid - free grid
- * @g: two dimensiton
+ * @grid: two dimensiton
  * @height: the height
  * Return void
  */
 void free_grid(int **grid, int height)
 {
 	int i = 0;
-	
+
 	for (; i < height; i++)
 		free(grid[i]);
 	free(grid);
