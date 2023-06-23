69fa739bb471a032cc175c0b1a21f74012c20dfd
yahia <yahia.mohammed.mahmoud@gmail.com>
2023-06-23 09:38:43 -0700

    yalla

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list p;
 	int i = n;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(p, n);
 	if (separator == NULL)
 		separator = "";
